225a86a20c8bc9313cb1bf8980096f5586579d33
Sowmya.C <rsowmya404@gmail.com>
2025-06-28 19:30:21 +0530

    Updated app.py to use dynamic port for Render

--- app.py
+++ app.py
@@ -52,6 +52,7 @@ def upload_file():
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-if __name__ == '__main__':
-    init_db()
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
